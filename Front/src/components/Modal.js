0b7bd289dc6066bdb541f5e1a3e17c00cfb14762
JFGaona <gaonajonathan5@gmail.com>
2024-09-23 18:19:57 -0500

    Cambios e implementación de Comprar y Confirmación de compra
    Se agrega un modal en js y css

---
+++ Front/src/components/Modal.js
@@ -0,0 +1,20 @@
+// src/components/Modal.js
+import React from 'react';
+import './Modal.css';
+
+const Modal = ({ show, onClose, children }) => {
+    if (!show) {
+        return null; // Si no está visible, no mostrar nada
+    }
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <button onClick={onClose} className="modal-close">X</button>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
