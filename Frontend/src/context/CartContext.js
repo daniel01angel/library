54902e641050d6975a79b642896b37b8e6c5532d
JFGaona <gaonajonathan5@gmail.com>
2024-11-13 23:28:49 -0500

    Carrito de Compras, con funcionalidad.

---
+++ Frontend/src/context/CartContext.js
@@ -0,0 +1,26 @@
+// src/context/CartContext.js
+import React, { createContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+    const [cart, setCart] = useState([]); // Estado inicial del carrito
+
+    const addToCart = (book) => {
+        setCart((prevCart) => [...prevCart, book]); // Agregar libro al estado del carrito
+        console.log("Libro agregado al carrito:", book); // Depuración
+        console.log("Estado del carrito después de agregar:", cart); // Depuración
+    };
+
+    const clearCart = () => {
+        setCart([]); // Vaciar carrito
+    };
+
+    return (
+        <CartContext.Provider value={{ cart, addToCart, clearCart }}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+export default CartProvider;
